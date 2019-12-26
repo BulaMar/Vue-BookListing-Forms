17329fe4323a1f33b8b2bcaf88ee518eff11658d
Marcin Buława <bulawa_marcin@wp.pl>
2019-12-26 19:24:37 +0100

    Fourth Page

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <h1>{{title}}</h1>
+    <input v-model="searchInput" placeholder="Search Books" type="text" />
     <ul>
-      <book-item v-for="book in books" :key="book.id" :book="book"></book-item>
+      <book-item v-for="book in searchedBooks" :key="book.id" :book="book"></book-item>
     </ul>
     <hr />
     <h2>Filtered Books By Ownership</h2>
@@ -31,6 +32,7 @@ export default {
       states: ["Want to Read", "Read", "Reading"],
       filters: ["borrowed", "bought"],
       holding: "bought",
+      searchInput: "",
       books: [
         {
           title: "Self-Reliance",
@@ -60,6 +62,12 @@ export default {
   computed: {
     filteredBooks() {
       return _.filter(this.books, ["ownership", this.holding]);
+    },
+    searchedBooks() {
+      const searchFilter = book => {
+        return book.title.toLowerCase().match(this.searchInput.toLowerCase());
+      };
+      return _.filter(this.books, searchFilter);
     }
   },
   methods: {
